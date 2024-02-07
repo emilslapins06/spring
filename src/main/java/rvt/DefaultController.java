dc572c214599dbfcb4b0e2bac6564a56e1e8a71c
emilslapins06 <145561786+emilslapins06@users.noreply.github.com>
2024-02-07 07:01:01 +0000

    07.02.2024

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -2,59 +2,59 @@ package rvt;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DefaultController {
 
-    /*@GetMapping(value = "/")
+    @GetMapping(value = "/")
     ModelAndView index() {
-        ArrayList<Student> students = new CsvManager(CsvManager.STUDENT_CSV).getAllStudents();
-
+        ArrayList<Student> students = new ArrayList<>();
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("students", students);
         modelAndView.addObject("date", new Date().toString());
         return modelAndView;
     }
-    */
-
-    @GetMapping(value = "/about")
-    ModelAndView about(@RequestParam HashMap<String, String> queryStringParams) {
-            ModelAndView modelAndView = new ModelAndView("about");
-
-            String id = queryStringParams.get("id");
-            String title = queryStringParams.get("title");
-            String description = queryStringParams.get("description");
-
-            return modelAndView;
-    }
 
     @GetMapping(value = "/test")
     public ModelAndView testAction() {
         ModelAndView modelAndView = new ModelAndView("test");
-
-        
         modelAndView.addObject("output1", 1);
-     
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/register")
+    public ModelAndView showRegistrationForm() {
+        ModelAndView modelAndView = new ModelAndView("registration/registration-page");
+        List<String> groups = new ArrayList<>(List.of("DP2-1", "DP2-2", "DP2-3", "DP2-4"));
+
+        Student student = new Student();
+        modelAndView.addObject("groups", groups);
+        modelAndView.addObject("student", student);
 
         return modelAndView;
     }
 
-    @RequestMapping(value = "/register")
-    public ModelAndView registerPage()
-    {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("registration");
-        return modelAndView;
-
+    @PostMapping(value = "/register")
+    public String registration(@ModelAttribute("student") Student student, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "registration/registration-page";
+        }
+        return "redirect:/register?success";
     }
 
 }
--- src/main/java/rvt/Student.java
+++ src/main/java/rvt/Student.java
@@ -1,104 +1,59 @@
 package rvt;
 
 import java.time.LocalDate;
-import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
+
+    @NotEmpty(message = "Vārds nevar būt tukšs")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Only letters are allowed")
     private String name;
+
+    
     private String surname;
+
     private int age;
+
     private String password;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
+
     private String gender;
+
     private boolean married;
+
     private String note;
+
+    @Email
     private String email;
+
     private String group;
-    
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    public String getSurname() {
-        return surname;
-    }
-    
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-    
-    public int getAge() {
-        return age;
-    }
-    
-    public void setAge(int age) {
-        this.age = age;
-    }
-    
-    public String getPassword() {
-        return password;
-    }
-    
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-    
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-    
-    public String getGender() {
-        return gender;
-    }
-    
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-    
-    public boolean isMarried() {
-        return married;
-    }
-    
-    public void setMarried(boolean married) {
-        this.married = married;
-    }
-    
-    public String getNote() {
-        return note;
-    }
-    
-    public void setNote(String note) {
-        this.note = note;
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-    
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    public String getGroup() {
-        return group;
-    }
-    
-    public void setGroup(String group) {
-        this.group = group;
-    }
-    
+
+   
     public String toString() {
-        return String.format("{%s, %s, %s, %s}", this.name, this.surname, this.email, this.group);
+        return String.format(
+                "{%s, %s, %s, %s, %s, %s, %s, %s, %s, %s}",
+                this.name,
+                this.surname,
+                this.age,
+                this.password,
+                this.birthday,
+                this.gender,
+                this.married,
+                this.note,
+                this.email,
+                this.group);
     }
 
 }
